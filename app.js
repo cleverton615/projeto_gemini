9aef37ba4abd5a9b991cabdebf353564e98f5ac3
CLEVERTON ALLAN DE MELO <CLEVERTON_ALLAN@OUTLOOK.COM>
2024-09-09 23:30:59 -0300

    ultima aula

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-console.log("Olá, Mundo!")
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa")
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+  
+    // Inicializa uma string vazia para armazenar os resultados
+    campoPesquisa = campoPesquisa.toLowerCase()
+    let resultados = ""
+    let titulo = ""
+    let descricao = ""
+    let tags = ""
+  
+    // Itera sobre os dados da pesquisa
+    for (let dado of dados) {
+      titulo = dado.titulo.toLowerCase()
+      descricao = dado.descricao.toLowerCase()
+      tags = dado.tags.toLowerCase()
+      // Constrói o HTML para cada resultado
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `
+        <div class="item-resultado">
+          <h2>${dado.titulo}</h2> <p class="descricao-meta">${dado.descricao}</p> <a href="${dado.link}" target="_blank">Saiba mais &rarr;</a> </div>`
+      } if (!campoPesquisa) { // isso é igual a colocar (campoPesquisa == "")
+        section.innerHTML = "<p>Nada foi encontrado</p>"
+        return
+      }
+    }
+    if (!resultados) { // isso é igual a colocar (resultados == "")
+      resultados = "<p>Nada foi encontrado</p>"
+    }
+    // Atribui o HTML construído à seção de resultados
+    section.innerHTML = resultados
+  }
---
+++ dados.js
@@ -0,0 +1,62 @@
+let dados = [
+    {
+        titulo : "One Piece",
+        descricao : "One Piece é um anime e mangá de enorme sucesso mundial, que acompanha as aventuras de Monkey D. Luffy, um jovem pirata que sonha em se tornar o Rei dos Piratas e encontrar o lendário tesouro One Piece.",
+        link : "https://pt.wikipedia.org/wiki/One_Piece",
+        tags: "onepiece zoro nami usopp"
+    },
+    {
+        titulo : "Naruto",
+        descricao : "Naruto é um anime e mangá que conta a história de um ninja adolescente chamado Naruto Uzumaki, que busca reconhecimento e sonha em se tornar o Hokage, o líder da sua aldeia.",
+        link : "https://pt.wikipedia.org/wiki/Naruto",
+        tags: "konoha sakura sasuke"
+    },
+    {
+        titulo : "Bleach",
+        descricao : "Bleach é um anime e mangá que narra as aventuras de Ichigo Kurosaki, um estudante do ensino médio que se torna um Shinigami substituto, responsável por proteger os humanos de espíritos malignos.",
+        link : "https://pt.wikipedia.org/wiki/Bleach_(mangá)",
+        tags: "seireitei"
+    },
+    {
+    titulo : "Fairy Tail",
+    descricao : "Fairy Tail é um anime e mangá que acompanha as aventuras de uma guilda de magos, a Fairy Tail, e seus membros, como Natsu Dragneel, Lucy Heartfilia e Gray Fullbuster.",
+    link : "https://pt.wikipedia.org/wiki/Fairy_Tail",
+    tags : "fairytail natsu lucy gray happy"
+  },
+  {
+    titulo: "Dragon Ball",
+    descricao: "Dragon Ball é um anime e mangá que acompanha as aventuras de Goku, um jovem saiyan que busca as Esferas do Dragão para realizar seus desejos e se tornar o guerreiro mais forte do universo.",
+    link: "https://pt.wikipedia.org/wiki/Dragon_Ball",
+    tags: "dragonball goku vegeta bulma shenron"
+  },
+  {
+    titulo: "Sword Art Online",
+    descricao: "Sword Art Online é um anime que se passa em um jogo de realidade virtual MMORPG onde os jogadores ficam presos e precisam completar o jogo para voltar à vida real.",
+    link: "https://pt.wikipedia.org/wiki/Sword_Art_Online",
+    tags: "swordartonline kirito asuna alicization"
+  },
+  {
+    titulo: "Jujutsu Kaisen",
+    descricao: "Jujutsu Kaisen acompanha Yuji Itadori, um estudante do ensino médio que, para salvar seus amigos, ingere um dedo amaldiçoado, tornando-se um recipiente para o poderoso feiticeiro Ryomen Sukuna. Agora, ele precisa equilibrar sua vida como estudante com as perigosas missões da escola de feitiçaria de Jujutsu.",
+    link: "https://pt.wikipedia.org/wiki/Jujutsu_Kaisen",
+    tags: "jujutsu, maldição, sukuna, gojo, tokyo"
+},
+{
+    titulo: "Demon Slayer: Kimetsu no Yaiba",
+    descricao: "Demon Slayer acompanha Tanjiro Kamado, um jovem que se torna um caçador de demônios após sua família ser massacrada e sua irmã transformada em um demônio. Ele busca uma cura para Nezuko e enfrenta diversas ameaças sobrenaturais.",
+    link: "https://pt.wikipedia.org/wiki/Kimetsu_no_Yaiba",
+    tags: "caçador de demônios, kimetsu no yaiba, tanjiro, nezuko, hashira, oni, respiração"
+},
+{
+    titulo: "My Hero Academia",
+    descricao: "My Hero Academia se passa em um mundo onde a maioria das pessoas possuem superpoderes, chamados de Individualidades. Izuku Midoriya, um garoto sem poderes, sonha em se tornar um herói e, após um encontro com All Might, o maior herói do mundo, ele herda o poder One For All e se matricula na U.A., uma escola para futuros heróis.",
+    link: "https://pt.wikipedia.org/wiki/Boku_no_Hero_Academia",
+    tags: "heroi, individualidade, u.a., all might, deku, vilão"
+},
+{
+    titulo: "Chainsaw Man",
+    descricao: "Chainsaw Man acompanha Denji, um jovem que vive em extrema pobreza e faz um pacto com seu cão demônio, Pochita. Após um incidente trágico, Denji se torna um híbrido humano-demônio, o Chainsaw Man, e se junta à caça aos demônios sob o comando da Pública Segurança.",
+    link: "https://pt.wikipedia.org/wiki/Chainsaw_Man",
+    tags: "chainsaw_man, demônio, denji, pochita, pública_segurança, ação"
+}
+]
